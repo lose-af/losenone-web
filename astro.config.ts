fb4613f76d3876ee72d93f22e41946c0a20ba06e
Nixieboluo <me@sagirii.me>
2023-08-25 23:20:53 +0800

    feat: move to new version (flint)

--- astro.config.ts
+++ astro.config.ts
@@ -1,23 +1,43 @@
-import mdx from '@astrojs/mdx';
-import solidJs from '@astrojs/solid-js';
-import tailwind from '@astrojs/tailwind';
-import icons from 'unplugin-icons/vite';
-// eslint-disable-next-line import/order
+import type { RawFlintConfig } from '@miracle-box/flint/utils/config';
+import flint from '@miracle-box/flint';
 import { defineConfig } from 'astro/config';
 
-export default defineConfig({
-	trailingSlash: 'always',
-	integrations: [mdx(), tailwind({ applyBaseStyles: false }), solidJs()],
-	markdown: {
-		shikiConfig: {
-			theme: 'github-light',
+const flintConfig: RawFlintConfig = {
+	locales: {
+		zh: {
+			label: '中文',
+		},
+		en: {
+			label: 'English',
 		},
 	},
-	vite: {
-		plugins: [
-			icons({
-				compiler: 'solid',
-			}),
-		],
+	defaultLocale: 'zh',
+	flintTranslationsPath: './i18n/index',
+	modules: {
+		news: {
+			type: 'news',
+			collectionId: 'news',
+			routeBasePath: 'news',
+			tags: {
+				updates: { name: 'Updates', desc: 'Update notes.' },
+				monthly: { name: 'Monthly', desc: 'Summary of every month.' },
+			},
+		},
+		docs: {
+			type: 'docs',
+			collectionId: 'docs',
+			routeBasePath: 'docs',
+		},
 	},
+	legacy: {
+		logoImg: '/favicon.png',
+		icpRecordText: '吉 ICP 备 2021002465 号',
+		icpRecordLink: 'https://beian.miit.gov.cn/',
+		secRecordText: '吉公网安备 22017202000263 号',
+		secRecordLink: 'https://www.beian.gov.cn/portal/registerSystemInfo?recordcode=22017202000263',
+	},
+};
+
+export default defineConfig({
+	integrations: [flint(flintConfig)],
 });
--- src/content/config.ts
+++ src/content/config.ts
@@ -1,23 +1,14 @@
-import { z, defineCollection } from 'astro:content';
+import { docsSchema, newsSchema } from '@miracle-box/flint/schema';
+import { defineCollection } from 'astro:content';
 
 const news = defineCollection({
-	schema: z.object({
-		title: z.string(),
-		desc: z.string(),
-		author: z.string(),
-		image: z.string().url(),
-		date: z.string().datetime(),
-		tags: z.string().array(),
-	}),
+	type: 'content',
+	schema: newsSchema,
 });
 
 const docs = defineCollection({
-	schema: z.object({
-		title: z.string(),
-		desc: z.string(),
-		outdated: z.boolean().default(false).optional(),
-		outdated_translation: z.union([z.string().url(), z.literal(false)]).optional(),
-	}),
+	type: 'content',
+	schema: docsSchema,
 });
 
 export const collections = {
